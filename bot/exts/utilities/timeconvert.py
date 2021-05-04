89614dd72e7b1d0faa7357f3a09056925df6004b
Kronifer <>
2021-05-04 17:21:58 +0000

    linted with black

--- bot/exts/utilities/timeconvert.py
+++ bot/exts/utilities/timeconvert.py
@@ -8,18 +8,24 @@ import discord
 from bot.utilities import get_yaml_val
 from bot.constants import DURATION_DICT
 
-colors = get_yaml_val('config.yml', 'colors')['colors']
+colors = get_yaml_val("config.yml", "colors")["colors"]
+
 
 class Times(commands.Cog):
     """Cog for Time commands."""
 
     def __init__(self, bot: commands.Bot):
         self.bot = bot
-    
-    @commands.command(aliases=['tz'])
-    async def timeintimezone(self, ctx: commands.Context, timezone: str = "US/Central", intime: Optional[str] = None):
+
+    @commands.command(aliases=["tz"])
+    async def timeintimezone(
+        self,
+        ctx: commands.Context,
+        timezone: str = "US/Central",
+        intime: Optional[str] = None,
+    ):
         """Gets time in specified timezone.
-        
+
         If an argument is given, the command returns what the time will be in the specified duration in the specified timezone."""
         strzone = timezone
         timezone = pytz.timezone(timezone)
@@ -28,7 +34,7 @@ class Times(commands.Cog):
             embed = discord.Embed(
                 title=f"Time in {strzone}:",
                 description=f"{timestamp.strftime('%I:%M %p')}",
-                color=colors["green"]
+                color=colors["green"],
             )
             await ctx.send(embed=embed)
         else:
@@ -37,14 +43,14 @@ class Times(commands.Cog):
             timestamp = seconds + utc
             timestamp = datetime.datetime.utcfromtimestamp(timestamp)
             timestamp = timestamp.astimezone(timezone)
-            timestamp = timestamp.strftime('%I:%M %p')
+            timestamp = timestamp.strftime("%I:%M %p")
             embed = discord.Embed(
                 title=f"Time in {strzone} in {intime}:",
                 description=timestamp,
-                color=colors["green"]
+                color=colors["green"],
             )
             await ctx.send(embed=embed)
-        
+
 
 def setup(bot: commands.Bot):
     """Loads cog."""
